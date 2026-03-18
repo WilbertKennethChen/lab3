e5c80cd99059daf2b648e545e9385193bf66a081
Wbeberthz <165393446+WilbertKennethChen@users.noreply.github.com>
2026-03-19 00:48:59 +0800

    Update new_expense.dart

--- widgets/new_expense.dart
+++ widgets/new_expense.dart
@@ -1,358 +1,191 @@
-import 'package:test_dart/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
-
-import 'package:test_dart/widgets/expenses_list/expenses_list.dart';
 import 'package:test_dart/models/expense.dart';
 
-class Expenses extends StatefulWidget {
-  const Expenses({super.key});
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
-  State<Expenses> createState() {
-    return _ExpensesState();
+  State<NewExpense> createState() {
+    return _NewExpenseState();
   }
 }
 
-class _ExpensesState extends State<Expenses> {
-  final List<Expense> _registeredExpenses = [
-    Expense(
-      title: 'Flutter Course',
-      amount: 19.99,
-      date: DateTime.now(),
-      category: Category.work,
-    ),
-    Expense(
-      title: 'Cinema',
-      amount: 15.69,
-      date: DateTime.now(),
-      category: Category.leisure,
-    ),
-  ];
+class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  final _key = GlobalKey<FormState>();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+  bool locked = false;
 
-  void _openAddExpenseOverlay() {
-    showModalBottomSheet(
-      isScrollControlled: true,
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
       context: context,
-      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
     );
-  }
-
-  void _addExpense(Expense expense) {
     setState(() {
-      _registeredExpenses.add(expense);
+      _selectedDate = pickedDate;
     });
   }
 
-  void _removeExpense(Expense expense) {
-    final expenseIndex = _registeredExpenses.indexOf(expense);
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController
+        .text); // tryParse('Hello') => null, tryParse('1.12') => 1.12
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (!_key.currentState!.validate() || _titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      // showDialog(
+      //   context: context,
+      //   builder: (ctx) => AlertDialog(
+      //     title: const Text('Invalid input'),
+      //     content: const Text(
+      //         'Please make sure a valid title, amount, date and category was entered.'),
+      //     actions: [
+      //       TextButton(
+      //         onPressed: () {
+      //           Navigator.pop(ctx);
+      //         },
+      //         child: const Text('Okay'),
+      //       ),
+      //     ],
+      //   ),
+      // );
+      setState(() {
+        locked = true;
+      });
+      return;
+    }
+
     setState(() {
-      _registeredExpenses.remove(expense);
+        locked = false;
     });
-    ScaffoldMessenger.of(context).clearSnackBars();
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        duration: const Duration(seconds: 3),
-        content: const Text('Expense deleted.'),
-        action: SnackBarAction(
-          label: 'Undo',
-          onPressed: () {
-            setState(() {
-              _registeredExpenses.insert(expenseIndex, expense);
-            });
-          },
-        ),
+
+    widget.onAddExpense(
+      Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        date: _selectedDate!,
+        category: _selectedCategory,
       ),
     );
+    Navigator.pop(context);
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget mainContent = const Center(
-      child: Text('No expenses found. Start adding some!'),
-    );
-
-    if (_registeredExpenses.isNotEmpty) {
-      mainContent = ExpensesList(
-        expenses: _registeredExpenses,
-        onRemoveExpense: _removeExpense,
-      );
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter ExpenseTracker'),
-        actions: [
-          IconButton(
-            onPressed: _openAddExpenseOverlay,
-            icon: const Icon(Icons.add),
-          ),
-        ],
-      ),
-      body: Column(
+    return Form(
+      key: _key,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
+        child: Column(
         children: [
-          Chartss(expenses: _registeredExpenses),
-          Expanded(
-            child: mainContent,
+          TextFormField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              label: Text('Title'),
+            ),
+            validator: (value) {
+              if (_titleController.text.trim().isEmpty) return "Input Name!!!" ;
+            },
           ),
-        ],
-      ),
-    );
-  }
-}
-
-class Chart extends StatelessWidget {
-  const Chart({super.key, required this.expenses});
-
-  final List<Expense> expenses;
-
-  double _getCategoryTotal(Category category) {
-    return expenses
-        .where((e) => e.category == category)
-        .fold(0.0, (sum, e) => sum + e.amount);
-  }
-
-  double get maxTotalExpense {
-    double max = 0;
-    for (var category in Category.values) {
-      final total = _getCategoryTotal(category);
-      if (total > max) {
-        max = total;
-      }
-    }
-    return max;
-  }
-
-  IconData _getIcon(Category category) {
-    switch (category) {
-      case Category.food:
-        return Icons.lunch_dining;
-      case Category.travel:
-        return Icons.flight_takeoff;
-      case Category.leisure:
-        return Icons.movie;
-      case Category.work:
-        return Icons.work;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(16),
-      child: Container(
-        height: 180,
-        padding: const EdgeInsets.all(12),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          gradient: LinearGradient(
-            colors: [
-              Theme.of(context)
-                  .colorScheme
-                  .primary
-                  .withValues(alpha: 0.2),
-              Theme.of(context)
-                  .colorScheme
-                  .primary
-                  .withValues(alpha: 0.05),
-            ],
-            begin: Alignment.bottomCenter,
-            end: Alignment.topCenter,
-          ),
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: Category.values.map((category) {
-            final total = _getCategoryTotal(category);
-            final fill = maxTotalExpense == 0
-                ? 0.0
-                : total / maxTotalExpense;
-
-            return Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 6),
-                child: Column(
+          const SizedBox(height: 16),
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    prefixText: '\$ ',
+                    label: Text('Amount'),
+                    border: OutlineInputBorder()
+                  ),
+                  validator: (value) {
+                    if (_amountController.text.trim().isEmpty || value == null) return "Input Amount!!!" ;
+                    final amounttoal = double.tryParse(value);
+                    if (amounttoal == null || amounttoal <= 0) return "Please input nums!!!" ;
+                  },
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    SizedBox(
-                      height: 100,
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: FractionallySizedBox(
-                          heightFactor: fill,
-                          widthFactor: 1,
-                          child: AnimatedContainer(
-                            duration: const Duration(milliseconds: 300),
-                            curve: Curves.easeOut,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(6),
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .primary
-                                  .withValues(alpha: 0.8),
-                            ),
-                          ),
-                        ),
+                    Text(
+                      _selectedDate == null
+                          ? 'No date selected'
+                          : formatter.format(_selectedDate!),
+                      style: TextStyle(color: locked && _selectedDate == null ? Theme.of(context).colorScheme.error : Theme.of(context).colorScheme.onSurface,)
+                    ),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(
+                        Icons.calendar_month,
                       ),
                     ),
-
-                    const SizedBox(height: 8),
-
-                    // ICON
-                    Icon(_getIcon(category)),
                   ],
                 ),
               ),
-            );
-          }).toList(),
-        ),
-      ),
-    );
-  }
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-class Charts extends StatelessWidget {
-  const Charts({super.key, required this.expenses});
-
-  final List<Expense> expenses;
-
-   IconData _getIcon(Category category) {
-    switch (category) {
-      case Category.food:
-        return Icons.lunch_dining;
-      case Category.travel:
-        return Icons.flight_takeoff;
-      case Category.leisure:
-        return Icons.movie;
-      case Category.work:
-        return Icons.work;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final categoryTotals = Category.values.map((cat) {
-      return expenses.where((e) => e.category == cat)
-                     .fold(0.0, (sum, e) => sum + e.amount);
-    }).toList();
-
-    final maxTotal = categoryTotals.reduce((a, b) => a > b ? a : b);
-
-    return Container(
-      margin: const EdgeInsets.all(16),
-      height: 180,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          for (var i = 0; i < Category.values.length; i++)
-            Expanded(
-              child: Column(
-                children: [
-                  Expanded(
-                    child: FractionallySizedBox(
-                      alignment: Alignment.bottomCenter,
-                      heightFactor: maxTotal == 0 ? 0 : categoryTotals[i] / maxTotal,
-                      child: AnimatedContainer(
-                        duration: const Duration(milliseconds: 300),
-                        margin: const EdgeInsets.symmetric(horizontal: 4),
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).colorScheme.primary,
-                          borderRadius: BorderRadius.circular(4),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Row(
+            children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(
+                          category.name.toUpperCase(),
                         ),
                       ),
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Icon(_getIcon(Category.values[i])), 
-                ],
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
               ),
-            ),
+              const Spacer(),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+              ElevatedButton(
+                onPressed: _submitExpenseData,
+                child: const Text('Save Expense'),
+              ),
+            ],
+          ),
         ],
+      ),
       ),
     );
   }
 }
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-class Chartss extends StatelessWidget{
-
-  const Chartss({super.key, required this.expenses});
-
-  final List<Expense> expenses;
-
-  IconData _getIcon(Category category) {
-    switch (category) {
-      case Category.food:
-        return Icons.lunch_dining;
-      case Category.travel:
-        return Icons.flight_takeoff;
-      case Category.leisure:
-        return Icons.movie;
-      case Category.work:
-        return Icons.work;
-    }
-  }
-double _getCategoryTotal(Category category) {
-  double sum = 0.0;
-  
-  for (final expense in expenses) {
-    if (expense.category == category) {
-      sum += expense.amount; 
-    }
-  }
-  
-  return sum; 
-}
-  @override
-Widget build(BuildContext context) {
-  double maxTotal = 0;
-  for (final category in Category.values) {
-    double sum = 0;
-    for (final expense in expenses) {
-      if (expense.category == category) {
-        sum += expense.amount;
-      }
-    }
-    if (sum > maxTotal) {
-      maxTotal = sum; 
-    }
-  }
-
-  return Card( child: Container(
-    height: 180, 
-    padding: const EdgeInsets.all(16),
-    child: Row(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        for (final category in Category.values)
-          Expanded(
-            child: Column(
-              children: [
-                Expanded( 
-                  child: FractionallySizedBox(
-                    alignment: Alignment.bottomCenter,
-                    heightFactor: maxTotal == 0 ? 0 : _getCategoryTotal(category) / maxTotal,
-                    child: AnimatedContainer(
-                      duration: const Duration(milliseconds: 300),
-                      margin: const EdgeInsets.symmetric(horizontal: 4),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.primary,
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 8),
-                Icon(_getIcon(category)),
-              ],
-            ),
-          ),
-      ],
-    ),
-  )
-);
-  }
-}
-
